f3a8a044779eb9d84c0528c824fe17df5732533b
laboratorio <laboratorio@UPOAULA10715>
2025-04-10 07:21:23 -0500

    Agregar archivos de proyecto.

---
+++ MoretaC-Ligas-Clase03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoretaC_Ligas_Clase03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
